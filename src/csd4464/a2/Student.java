33b892b17dbb79a0c167ee1b334e1f68f823a2b1
Savanna-Rae Furtah <savannarae@live.ca>
2017-01-18 11:41:09 -0500

    added Student class

---
+++ src/csd4464/a2/Student.java
@@ -0,0 +1,96 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package csd4464.a2;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author c0656308
+ */
+public class Student {
+    String name;
+    String id;
+    String gender;
+    double grade;
+
+    public Student() {
+    }
+
+    public Student(String name, String id, String gender, double grade) {
+        this.name = name;
+        this.id = id;
+        this.gender = gender;
+        this.grade = grade;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public double getGrade() {
+        return grade;
+    }
+
+    public void setGrade(double grade) {
+        this.grade = grade;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" + "name=" + name + ", id=" + id + ", gender=" + gender + ", grade=" + grade + '}';
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Student other = (Student) obj;
+        if (Double.doubleToLongBits(this.grade) != Double.doubleToLongBits(other.grade)) {
+            return false;
+        }
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        if (!Objects.equals(this.gender, other.gender)) {
+            return false;
+        }
+        return true;
+    }
+    
+}
+   
+   
