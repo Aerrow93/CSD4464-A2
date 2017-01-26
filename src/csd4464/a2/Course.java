43a456819ec982d367da215493b55ef4421417cc
Savanna-Rae <Savanna-Rae@Blue>
2017-01-25 21:50:39 -0500

    fixed course toString()

--- src/csd4464/a2/Course.java
+++ src/csd4464/a2/Course.java
@@ -13,6 +13,8 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
 import java.util.TreeMap;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 
 /**
  *
@@ -175,6 +177,16 @@ public class Course {
     //MATT This was originally typed at the top of the class
     @Override
     public String toString() {
-        return "Course{" + "students=" + students + '}';
+        JSONArray jArray = new JSONArray();
+        for (Student student : students){
+            JSONObject jObject = new JSONObject();
+            jObject.put("gender", student.getGender());
+            jObject.put("grade", student.getGrade());
+            jObject.put("name", student.getName());
+            jObject.put("id", student.getId());
+            jArray.add(jObject);
+        }
+        return jArray.toJSONString();
+        
     }
 }
